4db2ff76d76d1f7de1c22f29e4009811d0881e28
maybaby <ybyang7@iflytek.com>
2022-06-03 12:02:43 +0800

    first

---
+++ demo-theme/src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the ifly-theme extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'ifly-theme',
+  requires: [IThemeManager],
+  autoStart: true,
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension ifly-theme is activated!');
+    const style = 'ifly-theme/index.css';
+
+    manager.register({
+      name: 'ifly-theme',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default extension;
